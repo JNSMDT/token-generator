d8870fb0c82881123f8272a1e83e997cc9b05d8d
Jan Schmidt <me@jnschmdt.de>
2021-05-10 03:13:26 +0200

    changed how the characterstring is generated, set default length to 30

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
-// Characters allowed for the pw, with twice the possibility to get a letter or number
-const CHARACTERSSTRING =
-	'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz01234567890123456789!“#$%&‘()*+,-./[|]^_`{}~:;<=>?@';
+const CHARACTORS_LETTERS_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //26
+const CHARACTORS_LETTERS_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'; //26
+const CHARACTORS_NUMBERS = '0123456789'; // 10
+const CHARACTORS_SPECIAL_1 = '*+,-_./:;@!?#$%&'; // 16
+const CHARACTORS_SPECIAL_2 = '“‘()[]^`{}~<=>'; // 14
+
+// create characterstring from letters, numbers and special characters
+// letters are triple weight, number 4 times the normal and special character double weighted
+
+const CHARACTERSSTRING = [
+	CHARACTORS_LETTERS_UPPERCASE,
+	CHARACTORS_LETTERS_UPPERCASE,
+	CHARACTORS_LETTERS_UPPERCASE,
+	CHARACTORS_LETTERS_LOWERCASE,
+	CHARACTORS_LETTERS_LOWERCASE,
+	CHARACTORS_LETTERS_LOWERCASE,
+	CHARACTORS_NUMBERS,
+	CHARACTORS_NUMBERS,
+	CHARACTORS_NUMBERS,
+	CHARACTORS_NUMBERS,
+	CHARACTORS_SPECIAL_1,
+	CHARACTORS_SPECIAL_1,
+	CHARACTORS_SPECIAL_2,
+	CHARACTORS_SPECIAL_2
+].join('');
+
 interface GeneratePasswordOptions {
 	length: number;
 }
 
-function generatePassword(options: GeneratePasswordOptions) {
+function generatePassword(options: GeneratePasswordOptions): string {
 	const { length } = options;
 	// initialize uintArray
 	const uIntArray = new Uint8Array(1);
@@ -30,8 +53,8 @@ function generatePassword(options: GeneratePasswordOptions) {
 async function handleRequest(request: Request): Promise<Response> {
 	// get querystring from request url
 	const { searchParams } = new URL(request.url);
-	// get pwlength from querystring (default is 20)
-	const pwLength = searchParams.get('length') ?? 20;
+	// get pwlength from querystring (default is 30)
+	const pwLength = searchParams.get('length') ?? 30;
 	// check if provided pwLength is a number
 
 	if (Number.isNaN(Number(pwLength))) {
