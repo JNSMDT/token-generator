25ff3bf4b7964876827a28068fb6d14f9555d0f3
Jan Schmidt <me@jnschmdt.de>
2021-08-08 02:05:30 +0200

    added password generation to client

--- packages/website/src/lib/password.ts
+++ packages/website/src/lib/password.ts
@@ -1,16 +1,76 @@
-const CHARACTORS_LETTERS_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //26
-const CHARACTORS_LETTERS_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'; //26
+/**
+ * CONSTANTS
+ */
+const CHARACTORS_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //26
+const CHARACTORS_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'; //26
 const CHARACTORS_NUMBERS = '0123456789'; // 10
 const CHARACTORS_SPECIAL = '*+,-_.()/@!?#$%&"\':;[]^`{}~<=>'; // 30
 
-interface genPWOpts {
-	forbiddenCharacters: string;
-	characterWeight: {
-		uppercase: number;
-		lowercase: number;
-		numbers: number;
-		special: number;
-	};
+const DEFAULT_WEIGHT = {
+	uppercase: 3,
+	lowercase: 3,
+	numbers: 3,
+	special: 1
+};
+
+const DEFAULT_GEN_OPTIONS: genPWOpts = {
+	forbiddenCharacters: '',
+	weight: DEFAULT_WEIGHT
+};
+
+/**
+ * INTERFACES
+ */
+
+interface CharacterWeight {
+	uppercase: number;
+	lowercase: number;
+	numbers: number;
+	special: number;
 }
 
-export function generatePassword(length: number, options: genPWOpts) {}
+interface genPWOpts {
+	forbiddenCharacters: string;
+	weight: CharacterWeight;
+}
+
+/**
+ * FUNCTIONS
+ */
+function generateCharacterString(forbidden = '', weight?: CharacterWeight): string {
+	if (!weight) {
+		weight = DEFAULT_WEIGHT;
+	}
+	const uppercaseString = CHARACTORS_UPPERCASE.repeat(weight.uppercase);
+	const lowercaseString = CHARACTORS_LOWERCASE.repeat(weight.lowercase);
+	const numbersString = CHARACTORS_NUMBERS.repeat(weight.numbers);
+	const specialString = CHARACTORS_SPECIAL.repeat(weight.special);
+
+	const characterString = uppercaseString + lowercaseString + numbersString + specialString;
+
+	const purgedCharacters = Array.from(characterString).filter(c => !forbidden.includes(c));
+	const purgedCharacterString = purgedCharacters.join('');
+	return purgedCharacterString;
+}
+
+function cryptoRand(): number {
+	const randomBuffer = new Uint32Array(1);
+	crypto.getRandomValues(randomBuffer);
+	return randomBuffer[0] / (0xffffffff + 1);
+}
+
+function getRandomINT(max): number {
+	return Math.ceil(cryptoRand() * max);
+}
+
+export function generatePassword(length = 30, options = DEFAULT_GEN_OPTIONS): string {
+	const { forbiddenCharacters, weight } = options;
+
+	const characterString = generateCharacterString(forbiddenCharacters, weight);
+	const passwordArray = new Array(length).fill(null).map(() => {
+		const randINT = getRandomINT(characterString.length);
+		return characterString.charAt(randINT);
+	});
+
+	return passwordArray.join('');
+}
