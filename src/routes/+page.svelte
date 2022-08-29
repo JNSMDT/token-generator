995744b75d0349f5061f5e82170ad9aaed621f0d
Jan Schmidt <hi@jnschmdt.dev>
2022-08-29 02:06:36 +0200

    moved to new sveltekit version and routing structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,11 +18,8 @@
 /** @typedef {import("types/internal").ModalOptions} ModalOptions */
 
 	// Import Types
-	import { dev } from '$app/env';
 
 	const title = dev ? '(dev) Webcrypto Token Generator' : 'Webcrypto Token Geenrator';
-	// FIXME: JSDOC for globals
-	// @ts-ignore
 	const packageVersion = __VERSION__;
 	/** @type {string} */
 	let password;
