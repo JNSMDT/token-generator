4633636d1f6bc3529c99538c3776fb142fcdf5c6
Jan Schmidt <hi@jnsmdt.dev>
2022-12-22 05:39:29 +0100

    style: linting
    fixed linting of setup.js in test

--- test/setup.js
+++ test/setup.js
@@ -4,12 +4,12 @@
 import { randomFillSync } from 'node:crypto';
 
 Object.defineProperty(global, 'crypto', {
-  value: {
+	value: {
 		/**
 		 *
 		 * @param {NodeJS.ArrayBufferView} buffer
 		 * @returns
 		 */
-    getRandomValues: (buffer) => randomFillSync(buffer)
-  }
+		getRandomValues: (buffer) => randomFillSync(buffer)
+	}
 });
